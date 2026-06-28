b56da1bc9ae091a2367507b880fe21dd99c5dec5
UnknownTehk <brendanjhnsn@outlook.com>
2026-06-28 15:25:40 -0400

    Updated Cors.go

--- API/cors.go
+++ API/cors.go
@@ -7,6 +7,8 @@ import (
 
 // spaFS wraps an http.FileSystem so that requests for missing files fall back
 // to index.html, which is required for client-side routing in a React SPA.
+// Directories are left alone so http.FileServer can look up index.html inside
+// them naturally (converting them here caused an infinite redirect on "/").
 type spaFS struct{ http.FileSystem }
 
 func (s spaFS) Open(name string) (http.File, error) {
@@ -14,19 +16,7 @@ func (s spaFS) Open(name string) (http.File, error) {
 	if os.IsNotExist(err) {
 		return s.FileSystem.Open("/index.html")
 	}
-	if err != nil {
-		return nil, err
-	}
-	stat, err := f.Stat()
-	if err != nil {
-		_ = f.Close()
-		return nil, err
-	}
-	if stat.IsDir() {
-		_ = f.Close()
-		return s.FileSystem.Open("/index.html")
-	}
-	return f, nil
+	return f, err
 }
 
 func corsMiddleware(frontendURL, environment string, next http.Handler) http.Handler {
